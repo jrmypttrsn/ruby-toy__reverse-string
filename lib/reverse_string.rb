32dc4d853b5c7676cab405e01f48607532cbe0fa
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-02-25 19:22:39 +0000

    Done

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,5 +1,13 @@
 def reverse_characters(str)
+    str = "#{str}"
+	str = str.split("").sort{|x, y| y <=> x }.join
+	return str
 end
+puts(reverse_characters("Today is a good day"))
 
 def reverse_words(str)
+    str = "#{str}"
+	str = str.split(" ").sort{|x, y| y <=> x }.join(" ")
+	return str
 end
+puts(reverse_words("Today is a good day"))
